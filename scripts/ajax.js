26337349b6153480d5ef88aa7b0dc71db07b9809
Alan Moraes Souza <alan@souza.eti.br>
2018-06-05 12:50:01 -0300

    Atualizando eastegg.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -33,9 +33,9 @@ var ajax = {
             }
         };
         http.open(method, url, true);
-        http.setRequestHeader("application", "webapp");
-        http.setRequestHeader("action", "webact");
-        http.setRequestHeader("token", "webtok");
+//        http.setRequestHeader("application", "webapp");
+//        http.setRequestHeader("action", "webact");
+//        http.setRequestHeader("token", "webtok");
         return http;
     },
     get: function (url, finish) {
