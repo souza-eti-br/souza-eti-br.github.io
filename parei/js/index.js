b01a4040fb9954e9179c645ad85470df4c94b1a2
Alan Moraes Souza <alan@souza.eti.br>
2018-06-20 00:45:28 -0300

    Atualizando eastegg.

--- parei/js/index.js
+++ parei/js/index.js
@@ -63,7 +63,7 @@ var app = {
         return app.formatNumber(date[3], 2) + ":" + app.formatNumber(date[4], 2) + ":" + app.formatNumber(date[5], 2) + "." + app.formatNumber(date[6], 3) + " " + app.formatNumber(date[2], 2) + "/" + app.formatNumber(date[1], 2) + "/" + app.formatNumber(date[0], 4);
     },
     showGeneric: function (id, text, value) {
-        var formatted = app.formatDecimal(value, 15 - (text.length - 7));
+        var formatted = app.formatDecimal(value, 16 - (text.length - 7));
         if (formatted) {
             document.getElementById(id).innerHTML = text + formatted;
         } else {
