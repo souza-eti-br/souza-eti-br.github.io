6bb552b9f15e614ec20f0b19a9bc0b5bf9640aad
Alan Moraes Souza <alan@souza.eti.br>
2018-06-30 16:19:48 -0300

    Atualizando eastegg.

--- parei/js/index.js
+++ parei/js/index.js
@@ -2,7 +2,7 @@ var app = {
     initialize: function () {
         app.calcule();
     },
-    moment: [2018, 6, 28, 1, 9, 10, 801],
+    moment: [2018, 6, 30, 4, 47, 58, 515],
     now: [0, 0, 0, 0, 0, 0, 0],
     formatNumber: function (number, size) {
         var format = "" + number;
