8ab1390cef6d0e9d8185502168649c38385f0b33
Alan Moraes Souza <alan@souza.eti.br>
2020-12-31 23:54:55 -0300

    Parei!

--- parei/js/index.js
+++ parei/js/index.js
@@ -1,10 +1,11 @@
-var showNow = true;
+var showNow = false;
 var textNow = "";
 var app = {
     initialize: function () {
         app.calcule();
     },
-    moment: [2020, 12, 22, 23, 43, 8, 438],
+    // "2020-12-31 23:53:46.848",
+    moment: [2020, 12, 31, 23, 53, 46, 848],
     now: [0, 0, 0, 0, 0, 0, 0],
     formatNumber: function (number, size) {
         var format = "" + number;
